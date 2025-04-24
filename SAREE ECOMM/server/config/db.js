940416c08605c6883ea0acf9bf7eb109a3b15b66
Sakthivel R <sakthivelravi9894@gmail.com>
2025-04-24 20:15:00 +0530

    Update db.js

--- SAREE ECOMM/server/config/db.js
+++ SAREE ECOMM/server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://sakthivelrdeveloper:Blaze@clusterecomm.qw4p6.mongodb.net/?retryWrites=true&w=majority&appName=ClusterEcomm', {
+    const conn = await mongoose.connect(process.env.MONGO_URI , {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
